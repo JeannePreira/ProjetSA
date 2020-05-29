8c145931736d26849ec16eb1b01290e987e7a7c2
JeannePreira <jeannepreira95@gmail.com>
2020-05-29 05:20:03 +0100

    MonProjet

---
+++ Quizz_Answer/pages/connexion.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="./public/css/index.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    
+     <div id="titre">WELCOME TO QUESTION AND ANSWER GAME</div>
+
+        
+            <div class="connexion_bloc col-xm-12 col-sm-10 col-lg-8">     
+            <div class="titre2">Le plaiser de joueur</div>   
+               <div class="row connexion">
+                   <div class="col-sm-6">
+                   
+                        <div class="form-blocs">    
+                            <form action="" method="post" id="form-connexion">
+                                
+                                    <div class="line-logo "></div>
+                                                        
+                                <div class="form-group">
+                                    <button  class="form-control btn-primary inscription">INSCRIPTION</button> 
+                                </div>
+                            </form>
+                        </div>
+                   </div>
+                   <div class="col-sm-6">
+                   
+                <div class="form-blocs">    
+                    <form action="" method="post" id="form-connexion">
+
+                        <div class="form-group">
+                            <label for="pseudo">PSEUDO</label>
+                            <div class="line-pseudo line-icon-login"></div>
+                            <input type="text" class="form-control pseudo" error="error-1" name="pseudo"  id="" placeholder="Login">
+                            <div class="line-error" id="error-1"></div>
+                        </div>
+
+                        <div class="form-group">
+                        <label for="password" class="text_pass">PASSWORD</label>
+                            <div class="line-pass line-icon-password"></div>
+                            <input type="password" class="form-control " error="error-2" name="password"  id="" placeholder="Password">
+                            <div class="line-error" id="error-2"></div>
+                        </div>
+                        
+                        <div class="form-group">
+                            <button  class="form-control btn-primary">CONNEXION</button> 
+                        </div>
+                       
+                    </form>
+                </div>
+                   </div>
+               </div>
+            </div> 
+            <div class="col-offset-2"></div>
+      
+</div>
+
+           
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>    
+</body>
+</html>
